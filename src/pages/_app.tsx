d41c84ae02328178b581401b6929e28004fd20e9
kaito <sea_rr10_ch@outlook.com>
2021-09-24 20:41:58 +0900

    🎉 initail commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react';
+import Amplify from 'aws-amplify';
+import { AmplifySignOut, AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import awsconfig from '../aws-exports';
+
+Amplify.configure(awsconfig);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [authState, setAuthState] = useState<AuthState>();
+  const [user, setUser] = useState<Object>();
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    })
+  }, [])
+  return authState === AuthState.SignedIn && user ? (
+    <div className="App">
+      <AmplifySignOut />
+      <h2>コンポーネント</h2>
+    </div>
+
+  ) : (
+    <AmplifyAuthenticator />
+  )
+}
+export default MyApp
