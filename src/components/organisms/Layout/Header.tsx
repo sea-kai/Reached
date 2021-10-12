1c78f4b30287d09bbed9e5ed2558efdda209f8f0
kaito <sea_rr10_ch@outlook.com>
2021-10-13 00:05:34 +0900

    ✨ imprement Header

---
+++ src/components/organisms/Layout/Header.tsx
@@ -0,0 +1,33 @@
+import React, { memo, VFC } from "react";
+import Link from 'next/link';
+
+export const Header: VFC = () => {
+    return (
+    <div className = 'flex justify-center items-center font-mono text-sm'>
+      <header>
+        <nav className="w-screen bg-gray-800">
+          <div className="flex items-center pl-8 h-14"></div>
+          <div className="flex space-x-4">
+            <Link href="/">
+              <a className="py-2 px-3 text-gray-300 hover:bg-gray-700 rounded">
+                Home
+              </a>
+            </Link>
+            <Link href="/OpenSpace">
+              <a className="py-2 px-3 text-gray-300 hover:bg-gray-700 rounded">
+                質問広場
+              </a>
+            </Link>
+            <Link href="/StudyGroup">
+              <a className="py-2 px-3 text-gray-300 hover:bg-gray-700 rounded">
+                勉強会
+              </a>
+            </Link>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default memo(Header);
