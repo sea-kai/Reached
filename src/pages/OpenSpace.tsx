e40f2ed2dc5daafe131fd936a648114c53571d87
kaito <sea_rr10_ch@outlook.com>
2021-10-12 21:14:48 +0900

    🌱 add template

---
+++ src/pages/OpenSpace.tsx
@@ -0,0 +1,5 @@
+import React, { VFC } from "react";
+
+export const OpenSpace: VFC = () => {
+  return <p>質問広場ページです</p>
+}
---
+++ src/pages/StudyGroup.tsx
@@ -0,0 +1,5 @@
+import React, { VFC } from "react";
+
+export const StudyGroup: VFC = () => {
+  return <p>勉強会ページです</p>
+}
