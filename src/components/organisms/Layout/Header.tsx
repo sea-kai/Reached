9d06f460116e979c34262bf8ce99d69fd4062833
kaito <sea_rr10_ch@outlook.com>
2021-10-15 23:30:44 +0900

    💄 change architecture of Header

--- src/components/organisms/Layout/Header.tsx
+++ src/components/organisms/Layout/Header.tsx
@@ -6,8 +6,7 @@ export const Header: VFC = () => {
     <div className = 'flex justify-center items-center font-mono text-sm'>
       <header>
         <nav className="w-screen bg-gray-800">
-          <div className="flex items-center pl-8 h-14"></div>
-          <div className="flex space-x-4">
+          <div className="flex pt-10 space-x-4">
             <Link href="/">
               <a className="py-2 px-3 text-gray-300 hover:bg-gray-700 rounded">
                 Home
