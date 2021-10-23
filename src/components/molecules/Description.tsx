00a7a7e32ec6afab14fd15c4400b1367345c10fe
kaito <sea_rr10_ch@outlook.com>
2021-10-24 01:43:05 +0900

    💄 update ui

--- src/components/molecules/Description.tsx
+++ src/components/molecules/Description.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export const Description = (props: Props): JSX.Element => {
   return (
-    <div className='container flex flex-col justify-center items-start '>
+    <div className='container flex flex-col justify-center items-start ml-10'>
       <div className='text-5xl font-bold'>{props.title}</div>
-      <div className='items-center'>{props.text}</div>
+      <div className='text-2xl'>{props.text}</div>
     </div>
   )
 }
