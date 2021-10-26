bc523b8e8eac0a877f38160d6e2da0e25e98bafd
kaito <sea_rr10_ch@outlook.com>
2021-10-27 03:58:35 +0900

    💬 add TopDescription

---
+++ src/assets/DescriptionData.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const TopDescription = {
+  title: "Reachedへようこそ",
+  text: "教員になりたい全ての方を応援するWebサイトです。"
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,14 @@ import type { NextPage } from 'next';
 
 import { Layout } from 'src/components/templates/Layout';
 import { Description } from 'src/components/molecules/Description';
+import {TopDescription} from 'src/assets/DescriptionData';
 
 const Home: NextPage = () => {
   return (
     <Layout title='Home'>
       <div className='justify-center items-center'>
         <div className='flex flex-row'>
-          <Description title='Reached' text='説明' />
+          <Description title={TopDescription.title} text={TopDescription.text} />
           <img src='/writingNote.webp' alt='test' width='40%'/>
         </div>
       </div>
