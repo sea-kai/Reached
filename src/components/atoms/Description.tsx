20eb7e7a53b17c0c50c34e436ee6f3f07ec0775d
kaito <sea_rr10_ch@outlook.com>
2021-10-29 21:47:35 +0900

    💄 adjust UI Top

--- src/components/atoms/Description.tsx
+++ src/components/atoms/Description.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const Description = (props: Props): JSX.Element => {
   return (
-    <div className='container flex flex-col justify-center items-start ml-10'>
+    <div className='flex flex-col justify-center items-start ml-10 w-auto'>
       <div className='text-5xl font-bold'>{props.title}</div>
       <div className='text-2xl'>{props.text}</div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ import {TopDescription} from 'src/assets/DescriptionData';
 const Home: NextPage = () => {
   return (
     <Layout title='Home'>
-      <div className='justify-center items-center'>
-        <div className='flex flex-row'>
+      <div className='justify-center items-center '>
+        <div className='flex flex-row justify-between'>
           <Description title={TopDescription.title} text={TopDescription.text} />
           <img src='/writingNote.webp' alt='test' width='40%'/>
         </div>
