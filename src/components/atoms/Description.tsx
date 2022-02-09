f7b3ddafa5cbf28c68328f3b92725427cc251d56
kaito <sea_rr10_ch@outlook.com>
2022-02-09 19:59:27 +0900

    design: adjust Home Description

--- src/components/atoms/Description.tsx
+++ src/components/atoms/Description.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 export const Description = (props: Props): JSX.Element => {
   return (
-    <div className='flex flex-col justify-center items-start ml-10 w-auto'>
-      <div className='text-5xl font-bold'>{props.title}</div>
-      <div className='text-2xl'>{props.text}</div>
+    <div className='flex flex-col items-start mt-16 ml-8 w-2/5'>
+      <h1 className='text-6xl font-bold'>{props.title}</h1>
+      <h2 className='my-8 text-2xl'>{props.text}</h2>
     </div>
   )
 }
--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -4,26 +4,17 @@ import Head from 'next/head';
 import Header from 'src/components/organisms/Layout/Header'
 import { LayoutType } from 'src/types';
 
-export const Layout = ({children, title = 'Reached'}: LayoutType): React.ReactElement=> {
+export const Layout = ({children}: LayoutType): React.ReactElement=> {
   return (
     <div className = 'flex flex-col justify-center items-center min-h-screen font-mono text-sm text-gray-600 bg-white'>
       <Head>
-        <title>{title}</title>
+        <title>Reached~教員になりたい人を応援するサイト~</title>
       </Head>
       <Header />
-      <main className='flex flex-col flex-1 mx-32 mt-24 '>
+      <main className='flex flex-col flex-1 mx-32 mt-16 '>
         {children}
       </main>
       <footer className='flex justify-center items-start w-full h-12 border-t'>
-        <a
-          className='flex items-center'
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-        Powered by{' '}
-        <img src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-        </a>
       </footer>
     </div>
   )
